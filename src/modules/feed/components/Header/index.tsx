ac97f98258369f892f279c1ca4c78466bcc629e1
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-08-04 07:22:36 +0700

    feature(feed): implement Header

---
+++ src/modules/feed/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div className="h-14 flex items-center pl-4">
+      <h1 className="font-bold text-xl">
+        ShareFree
+      </h1>
+    </div>
+  )
+}
+
+export default Header
